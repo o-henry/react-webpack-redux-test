f5918f8aa7da6c8da04fdb49e8da7fc4e139fe5c
henry <cksgod16@gmail.com>
2021-04-26 03:22:26 +0900

    Init

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+const presets = ["@babel/preset-react", "@babel/preset-env"];
+const plugins = [
+  "@babel/plugin-transform-template-literals",
+  "@babel/plugin-transform-arrow-functions",
+];
+
+module.exports = { presets, plugins };
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.ts"),
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};

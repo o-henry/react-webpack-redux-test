6f7adf15d0a42e54b954ab9ff093b3f99423b16e
henry <cksgod16@gmail.com>
2021-04-26 10:36:30 +0900

    Add Eslint & Fix webpack production & development

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
 
 module.exports = {
+  mode,
   entry: path.resolve(__dirname, "./src/index.tsx"),
   devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: "ts-loader",
       },
@@ -20,4 +29,34 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].css",
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "production" ? "" : "[DEV]",
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
+    new ESLintPlugin({
+      extensions: [".tsx", ".ts", ".js"],
+      exclude: "node_modules",
+    }),
+  ],
 };

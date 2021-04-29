d98a15eaa3c2a09f877fcb8a81c51164afb19266
henry <cksgod16@gmail.com>
2021-04-29 22:48:42 +0900

    Add @testing-library/react

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,15 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      plugins: config.plugins.filter((plugin) => {
+        if (plugin.constructor.name === 'ESLintWebpackPlugin') {
+          return false;
+        }
+        return true;
+      }),
+    };
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/setUpTests.ts'],
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
   },

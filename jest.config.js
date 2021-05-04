0cea51e5142d3388f301dffade4d6946bac529cf
Chan Haeng Lee <48753593+o-henry@users.noreply.github.com>
2021-05-04 21:51:40 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',

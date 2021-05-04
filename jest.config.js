b53862d59f00b0706a598e2729f1173459843474
Chan Haeng Lee <48753593+o-henry@users.noreply.github.com>
2021-05-04 12:53:11 +0900

    Update jest.config.js
    Fix setupenv directory

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/setUpTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setUpTests.ts'],
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
   },
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
 };

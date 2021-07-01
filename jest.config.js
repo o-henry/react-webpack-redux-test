6d7a75b2e465416758db67c0e0f4537739f9951d
henry <cksgod16@gmail.com>
2021-07-01 16:58:32 +0900

    Update monorepo

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,27 @@
+const path = require('path');
+const { lstatSync, readdirSync } = require('fs');
+
+// get listing of packages in mono repo
+const basePath = path.resolve(__dirname, 'packages');
+const packages = readdirSync(basePath).filter((name) => lstatSync(path.join(basePath, name)).isDirectory());
+
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: ['jsdom', 'node'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
+    '^.+\\.(css|less|scss)$': 'babel-jest',
+    // automatically generated list of our packages from packages directory.
+    // will tell jest where to find source code for @ahoopen/ packages, it points to the src instead of dist.
+    ...packages.reduce(
+      (acc, name) => ({
+        ...acc,
+        [`@ahoopen/${name}(.*)$`]: `<rootDir>/packages/./${name}/src/$1`,
+      }),
+      {},
+    ),
   },
+  modulePathIgnorePatterns: [...packages.reduce((acc, name) => [...acc, `<rootDir>/packages/${name}/dist`], [])],
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
 };
